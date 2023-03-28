1a5984b600589475c52ff1af2d8a5005871c2744
Yeong joon <bluehome8626@naver.com>
2023-03-28 23:31:55 +0900

    feat : 학교폭력 관련 기관 페이지 퍼블리싱

--- user/src/pages/BuildingPage.tsx
+++ user/src/pages/BuildingPage.tsx
@@ -1,7 +1,44 @@
 import styled from "styled-components";
+import logo1 from "../assets/logo1.svg";
+import Building from "../components/building";
+import { Institution } from "../constants/Institution";
 
 const BuildingPage = () => {
-  return <>a</>;
+  return (
+    <Wrapper>
+      <img src={logo1} alt="logo" />
+      <BoxWrapper>
+        {Institution.map((item) => (
+          <Building
+            title={item.title}
+            call={item.call}
+            description={item.description}
+          />
+        ))}
+      </BoxWrapper>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  > img {
+    width: 88px;
+    margin-top: 28px;
+    margin-left: 18px;
+    margin-bottom: 40px;
+  }
+`;
+
+const BoxWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  overflow-y: scroll;
+`;
 
 export default BuildingPage;
